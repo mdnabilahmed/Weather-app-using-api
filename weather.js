e75ddc60b42fe91fb8a828610b6dc76da1bb8690
MDNabilAhmed <mdnabilahmed706@gmail.com>
2025-07-10 02:40:02 +0530

    if else picture changes with how weather is

--- weather.js
+++ weather.js
@@ -1,10 +1,16 @@
+const images = [
+  "./images/sunny.png",
+  "./images/rain.png",
+  "./images/cloud.png",
+  "./images/thunder.png",
+];
+
 async function city(name) {
   let response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=58a86effaffcce194dae905c36101345`
   );
 
   let data = await response.json();
-  console.log(data.wind.speed);
 
   document.querySelector(".city").innerHTML = name.toUpperCase();
   document.querySelector(".temp").innerHTML =
@@ -12,6 +18,23 @@ async function city(name) {
   document.getElementsByClassName("humidity")[0].innerHTML = data.main.humidity;
   document.getElementsByClassName("wind")[0].innerHTML =
     data.wind.speed * 3.6 + " KM/h";
+
+  let value = data.weather[0].main;
+  let wedh = document.getElementsByClassName("weather-icon")[0];
+
+  console.log(value);
+
+  if (value === "Clouds") {
+    wedh.src = "images/clouds.png";
+  } else if (value === "Clear") {
+    wedh.src = "images/clear.png";
+  } else if (value === "Rain") {
+    wedh.src = "images/rain.png";
+  } else if (value === "Wind") {
+    wedh.src = "images/wind.png";
+  } else {
+    wedh.src = "images/snow.png";
+  }
 }
 
-city("patna");
+city("Ichhāwar");
